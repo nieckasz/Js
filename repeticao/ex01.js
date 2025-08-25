5251d729d8a4f07d719628f949b2971a88f1a545
Bruno <165843243+nieckasz@users.noreply.github.com>
2025-08-25 17:48:43 -0300

    tabuada
    laços de repetiçao

---
+++ repeticao/ex01.js
@@ -0,0 +1,16 @@
+var cont
+cont=0
+while(cont<2){
+    alert("Irritando o usuário com white "+cont+" vezes")
+    cont++
+}
+for(cont=0;cont<2;cont++)
+{
+alert("Agora irritando com o for "+cont+" vezes")
+
+}
+var resposta
+do{
+    resposta=confirm("Deseja Continuar?")
+}while(resposta==true)
+    
---
+++ repeticao/ex02.js
@@ -0,0 +1,9 @@
+var tab,n,r
+tab=parseInt(prompt("digite o numero para a tubuada:"))
+for(n=0;n<=10;n++)
+{
+    r=tab*n
+
+document.write("<h2>"+ tab+"x"+n+"="+r+"</h2>")
+}
+
