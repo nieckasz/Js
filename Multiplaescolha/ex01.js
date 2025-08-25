e28d52943e296d24c6c057eccacabe15f1280cf5
Bruno <165843243+nieckasz@users.noreply.github.com>
2025-08-25 17:11:58 -0300

    multipla escolha
    domingo

---
+++ Multiplaescolha/ex01.js
@@ -0,0 +1,27 @@
+var dia
+dia=parseInt(prompt("digite o numero corespondente ao dia de semana:"))
+switch(dia){
+
+    case 1 :{alert("domingo") 
+    break}
+case 2 :{alert("segunda") 
+    break}
+case 3 :{alert("terça") 
+    break}
+case 4 :{alert("quarta") 
+    break}
+case 5 :{alert("quinta") 
+    break}
+case 6 :{alert("sexta") 
+    break}
+case 7 :{alert("sábado") 
+    break}
+    default:{
+        alert("Opção Inválida")
+        break
+    }
+
+
+
+
+}
